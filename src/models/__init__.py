ec3723b941a0e965e6580cb538d783d67cba30a3
rrequeena <rprequenac@gmail.com>
2026-01-28 13:00:00 -0500

    fix(models): Add model exports to __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,7 @@
 # src/models/__init__.py
+
+from src.models.document import Document
+from src.models.extraction import ExtractionResult
+from src.models.approval import ApprovalRequest
+
+__all__ = ["Document", "ExtractionResult", "ApprovalRequest"]
